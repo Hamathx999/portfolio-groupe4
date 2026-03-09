8a0bc0e718c7e419404608e6ce9ec05033813639
Hamathx999 <hamath.o.diallo18@gmail.com>
2026-03-09 20:03:17 +0000

    commit de hamath

--- js/cv-hamath.js
+++ js/cv-hamath.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     bouton.addEventListener("click", function() {
       const lien=document.createElement('a');
 
-        lien.href="../CV/CV De Hamath.pdf";
+        lien.href="CV/CV De Hamath.pdf";
         lien.download="CV De Hamath";
         lien.click();
         alert("Le cv est telechargé !");
